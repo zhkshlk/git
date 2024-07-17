7716ea5332a8de141fcb0798ee7627c6adf8bdf0
Zhakshylyk <zhakshylykk@gmail.com>
2024-07-17 20:22:56 +0600

    первое изменение

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Мы сегодня изучаем версионность - Git");
+        System.out.println("Добавил новую строку для демонстрации");
+    }
+}
